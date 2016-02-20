6ae4b50429333130fae3385e4eda8e6da90d99ab
Hugh Nguyen <hugh.hn@gmail.com>
2016-02-19 21:45:22 -0800

    change tint color for icons

--- HugoTwitter/TweetCell.swift
+++ HugoTwitter/TweetCell.swift
@@ -16,6 +16,10 @@ class TweetCell: UITableViewCell {
     @IBOutlet weak var timestampLabel: UILabel!
     @IBOutlet weak var tweetLabel: UILabel!
     @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var retweetedIcon: UIImageView!
+    @IBOutlet weak var replyIcon: UIImageView!
+    @IBOutlet weak var retweetIcon: UIImageView!
+    @IBOutlet weak var favIcon: UIImageView!
     
     @IBOutlet weak var profileImageTopMargin: NSLayoutConstraint!
     
@@ -33,6 +37,18 @@ class TweetCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
         profileImageView.layer.cornerRadius = 5
+        
+        retweetedIcon.image = retweetedIcon.image!.imageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate)
+        retweetedIcon.tintColor = UIColor.grayColor()
+        
+        replyIcon.image = replyIcon.image!.imageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate)
+        replyIcon.tintColor = UIColor.grayColor()
+        
+        retweetIcon.image = retweetIcon.image!.imageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate)
+        retweetIcon.tintColor = UIColor.grayColor()
+        
+        favIcon.image = favIcon.image!.imageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate)
+        favIcon.tintColor = UIColor.grayColor()
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
