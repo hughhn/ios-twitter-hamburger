3d0d885329ab958b14a68eb118c05fbe8117dcaa
Hugh Nguyen <hugh.hn@gmail.com>
2016-02-28 23:20:47 -0800

    menu styling

--- HugoTwitter/HamburgerViewController.swift
+++ HugoTwitter/HamburgerViewController.swift
@@ -69,7 +69,7 @@ class HamburgerViewController: UIViewController {
     func toggleHamburger(open: Bool) {
         UIView.animateWithDuration(0.3) { () -> Void in
             if open {
-                self.leftMarginConstraint.constant = self.view.frame.size.width - 100
+                self.leftMarginConstraint.constant = self.view.frame.size.width - 200
             } else {
                 self.leftMarginConstraint.constant = 0
             }
